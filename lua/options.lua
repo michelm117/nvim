726b64860dc30d56b59a5d1d6b0eabc3563caed8
Marc Michel <marc@michel.lu>
2024-04-12 23:28:58 +0200

    chore: minimal setup

---
+++ lua/options.lua
@@ -0,0 +1,33 @@
+vim.g.mapleader = " "
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.wrap = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- https://gist.github.com/andersevenrud/015e61af2fd264371032763d4ed965b6
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+vim.opt.encoding = "UTF-8"
+vim.opt.colorcolumn = "120"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.filetype.add({ extension = { templ = "templ" } })
+
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/plugins/vim-foaterm.lua
@@ -0,0 +1,7 @@
+return {
+	{
+		"voldikss/vim-floaterm",
+		lazy = false,
+	},
+}
+-- vim: ts=2 sts=2 sw=2 et
