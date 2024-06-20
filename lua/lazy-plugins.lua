0173d73609bb0a7161a625b4a595c55469e0839a
Michel, Marc - D0327901 <marc.michel@dm.de>
2024-06-20 17:18:53 +0200

    feat: add oil

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -41,6 +41,7 @@ require("lazy").setup({
 	require("plugins/bufferline"),
 	require("plugins/todo-comments"),
 	require("plugins/mini"),
+	require("plugins/oil"),
 }, {
 	ui = {
 		-- If you are using a Nerd Font: set icons to an empty table which will use the
