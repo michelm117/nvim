9ea30dfa81889f64d90f13162924ff2bee9fc95d
Michel, Marc - D0327901 <marc.michel@dm.de>
2024-09-18 10:15:39 +0200

    feat: add autotag plugin
    auto close and rename html tags

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -42,5 +42,23 @@ return {
 			--    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 		end,
 	},
+	{ -- Auto close and rename HTML tags
+		"windwp/nvim-ts-autotag",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("nvim-ts-autotag").setup({
+				opts = {
+					enable_close = true, -- Auto close tags
+					enable_rename = true, -- Auto rename pairs of tags
+					enable_close_on_slash = false, -- Auto close on trailing </
+				},
+				per_filetype = {
+					["html"] = {
+						enable_close = false,
+					},
+				},
+			})
+		end,
+	},
 }
 -- vim: ts=2 sts=2 sw=2 et
